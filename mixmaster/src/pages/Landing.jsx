1c7966b15e2c75c0ab0a3de0650aa5f61c40c1f0
Trần Nguyên <trandangnguyen125@gmail.com>
2024-09-28 12:52:22 +0700

    Upload MixMaster

---
+++ mixmaster/src/pages/Landing.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React from "react";
+import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
+import { useLoaderData } from "react-router-dom";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+
+    const searchTerm = url.searchParams.get("search") || "";
+    await queryClient.ensureQueryData(searchCocktailsQuery(searchTerm));
+
+    return { searchTerm };
+  };
+
+const Landing = () => {
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm));
+
+  return (
+    <>
+      <SearchForm searchTerm={searchTerm} />
+      <CocktailList drinks={drinks} />
+    </>
+  );
+};
+
+export default Landing;
